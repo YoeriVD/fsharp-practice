a615c28937fb3f910ad1231a3b50087fad7f3161
yoerivd <yoeri.vandamme@gmail.com>
2023-05-16 00:44:48 +0200

    a little help from copilot

--- fsharp-practice/Bmi.fs
+++ fsharp-practice/Bmi.fs
@@ -15,4 +15,23 @@ let evaluateBmi bmi =
     | bmiValue when bmiValue >= 25.0 && bmiValue <= 29.9 -> "Overgewicht"
     | _ -> "Gezondheidsrisico"
     
-let bmi weight heigth = calculateBmi weight heigth |> evaluateBmi
+// define a type for the bmi
+type Bmi = {
+    weight: float
+    height: float
+    bmi: float
+    evaluation: string
+}
+
+// define a function to calculate the bmi, reuse existing functions
+let bmi weight height =
+    let bmi = calculateBmi weight height
+    let evaluation = evaluateBmi bmi
+    { weight = weight; height = height; bmi = bmi; evaluation = evaluation }
+
+// define a function to pretty print the bmi
+let printBmi bmi =
+    printfn "Gewicht: %f" bmi.weight
+    printfn "Lengte: %f" bmi.height
+    printfn "BMI: %f" bmi.bmi
+    printfn "Evaluatie: %s" bmi.evaluation
--- fsharp-practice/Program.fs
+++ fsharp-practice/Program.fs
@@ -2,9 +2,15 @@
 
 
 open Bmi
-    
+
 [<EntryPoint>]
-let main args =
-    bmi 81 1.74
-    |> printf "%s"
-    0
+let rec promptUser args =
+    printfn "Enter your weight in kilograms: "
+    let weight = System.Console.ReadLine() |> float
+    printfn "Enter your height in meters: "
+    let height = System.Console.ReadLine() |> float
+    bmi weight height |> printBmi
+    printfn "Would you like to calculate another BMI? (y/n)"
+    let response = System.Console.ReadLine()
+    if response = "y" then promptUser args
+    else 0 // exit code
